9027ff4ad2210c5cc5997e90d1472226232d31f6
Luke Edwards <luke.edwards05@gmail.com>
2019-03-22 13:58:58 -0700

    golf: extract `reduce` helper

--- src/index.js
+++ src/index.js
@@ -6,30 +6,23 @@ function toChars(str) {
 	return arr;
 }
 
-export function encode(key, value) {
-	var i=0, j=0, str='';
-	var hash = toChars(key);
-	var chars = toChars(value);
-	for (; i < chars.length; i++) {
-		for (j=0; j < hash.length; j++) {
-			chars[i] ^= hash[j];
-		}
-		str += ('0' + chars[i].toString(16)).substr(-2);
+function reduce(arr, value, fn) {
+	var j, tmp, str='', chars=toChars(value);
+	while (tmp = chars.shift()) {
+		for (j=0; j < arr.length; j++) tmp ^= arr[j];
+		str += fn(tmp);
 	}
 	return str;
 }
 
+export function encode(key, value) {
+	return reduce(toChars(key), value, function (x) {
+		return ('0' + x.toString(16)).substr(-2);
+	});
+}
 
 export function decode(key, value) {
-	var i=0, j=0, str='';
-	var hash = toChars(key);
-	var chars = value.match(/.{1,2}/g);
-	for (; i < chars.length; i++) {
-		chars[i] = parseInt(chars[i], 16);
-		for (j=0; j < hash.length; j++) {
-			chars[i] ^= hash[j];
-		}
-		str += String.fromCharCode(chars[i]);
-	}
-	return str;
+	return reduce(toChars(key), value, function (x) {
+		return String.fromCharCode(x);
+	});
 }
