2f5705ce2eb84b7670b6016c47da88dc33d9a327
Luke Edwards <luke.edwards05@gmail.com>
2019-03-24 15:32:25 -0700

    chore: add tests

--- test/index.js
+++ test/index.js
@@ -8,3 +8,47 @@ test('exports', t => {
 	t.end();
 });
 
+test('encode()', t => {
+	let key = 'foobar';
+
+	let foo = lib.encode(key);
+	t.is(typeof foo, 'function', 'returns a new function');
+
+	let val1 = foo('hello');
+	t.is(typeof val1, 'string', '~> factory produces a string');
+
+	let val2 = foo('world');
+	t.is(typeof val2, 'string', '~> factory produces a string');
+
+	t.isNot(val1, val2, 'hashed-values are not the same');
+	t.is(val1, '7f727b7b78', '~> 1st value is correct');
+	t.is(val2, '6078657b73', '~> 2nd value is correct');
+
+	let val3 = lib.encode('hello')('hello');
+	t.isNot(val1, val3, 'result is different w/ new key');
+	t.is(val3, '0a070e0e0d', '~> 3rd value is correct');
+
+	t.end();
+});
+
+test('decode()', t => {
+	let key = 'foobar';
+
+	let foo = lib.decode(key);
+	t.is(typeof foo, 'function', 'returns a new function');
+
+	let val1 = foo('7f727b7b78');
+	t.is(typeof val1, 'string', '~> factory produces a string');
+
+	let val2 = foo('6078657b73');
+	t.is(typeof val2, 'string', '~> factory produces a string');
+
+	t.isNot(val1, val2, 'real-values are not the same');
+	t.is(val1, 'hello', '~> 1st value is correct');
+	t.is(val2, 'world', '~> 2nd value is correct');
+
+	let val3 = lib.decode('hello')('0a070e0e0d');
+	t.is(val3, 'hello', '~> 3rd value is correct');
+
+	t.end();
+});
